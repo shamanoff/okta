773e52643affd1f32604ebbfd5d8f0141c7a7c56
Michael <shamanoff@gmail.com>
2017-04-01 21:28:13 +0300

    Initial commit

---
+++ src/main/java/com/testsaml/TestsamlApplication.java
@@ -0,0 +1,12 @@
+package com.testsaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsamlApplication.class, args);
+	}
+}
